7b7fd2b876bdef125f93b6059c0ad9a86631ee08
Jesse Clark <jesse.clark@unco.edu>
2016-07-15 09:31:55 -0600

    Add size json file.

---
+++ gadget/dist/settings/size.js
@@ -0,0 +1,20 @@
+{
+	"size" : [
+		{
+			"name" : "Small",
+			"class" : "icon-sm"
+		},
+		{
+			"name" : "Medium",
+			"class" : "icon-md"
+		},
+		{
+			"name" : "Large",
+			"class" : "icon-lg"
+		},
+		{
+			"name" : "Extra Large",
+			"class" : "icon-xl"
+		}
+	]
+}
