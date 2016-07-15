977f7db667ef43b0b660463c1c689c26c8561385
Jesse Clark <jesse.clark@unco.edu>
2016-07-15 09:28:59 -0600

    Start building out icons json.

---
+++ gadget/dist/settings/icons.js
@@ -0,0 +1,23 @@
+{
+	"icons": [
+		{
+			"name"  : "accessibility",
+			"class" : "accessibility",
+			"group" : "Everyone"
+		},
+		{
+			"name" : "accessible",
+			"class" : "accessible"
+		},
+		{
+			"name" : "account balance",
+			"class" : "account_balance",
+			"group" : "Everyone"
+		},
+		{
+			"name" : "account balance wallet",
+			"class" : "account_balance_wallet",
+			"group" : "Everyone"
+		}
+	]
+}
