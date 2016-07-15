3bbcba9933434951ee9802410567e48412081f62
Jesse Clark <jesse.clark@unco.edu>
2016-07-15 09:05:22 -0600

    Create basic struture for gadget.

--- css/README.md
+++ css/README.md
@@ -1,3 +1,18 @@
 # CSS
 
 This section contains css that should be liked to or added to your University's CSS file.
+
+
+## Icon Font
+
+Google provides the icons as a font. Instructions on how to install that are located [on their site](http://google.github.io/material-design-icons/).
+
+At a bare minimum, add this:
+
+```
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+```
+
+## Customizations
+
+The code below is additional code that is used for sizes and colors of the icons.
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+
+//for testing:
+var mocha = require('gulp-mocha');
+var concat = require('gulp-concat');
+
+//default task:
+gulp.task('default', ['sass', 'scripts']);
+gulp.task('build', ['sass', 'scripts', 'watch']);
+
+// watchers:
+gulp.task('watch', function() {
+
+  gulp.watch('gadget/src/scss/*.scss', ['sass']);  
+  gulp.watch(['gadget/src/js/*.js', 'gadget/test/*.js'], ['scripts', 'test']);
+
+});
+
+gulp.task('sass', function () {
+  gulp.src(['./gadget/src/scss/app.scss'])
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(concat('gadget.css'))
+    .pipe(gulp.dest('./gadget/dist/css/'))
+    .pipe(minifyCSS())
+    .pipe(rename("gadget.min.css"))
+    .pipe(gulp.dest('./gadget/dist/css/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['./gadget/src/gadgetlib.js', './gadget/src/js/*.js' ])
+    .pipe(concat('gadget.js'))
+    .pipe(gulp.dest('./gadget/dist/js/'))
+    .pipe(uglify())
+    .pipe(rename("gadget.min.js"))
+    .pipe(gulp.dest('./gadget/dist/js/'));
+});
+
+gulp.task('test', ['scripts'], function () {
+  return gulp.src('gadget/test/*.js', {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it 
+    .pipe(mocha({reporter: 'nyan'}));
+});
